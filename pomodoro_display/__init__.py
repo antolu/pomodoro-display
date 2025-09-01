16ead0da0e72c5b71344c8c402b3d2060cc52c25
Anton Lu <anton.lu@cern.ch>
2025-09-01 14:00:00 +0200

    update __init__

--- pomodoro_display/__init__.py
+++ pomodoro_display/__init__.py
@@ -1,1 +1,10 @@
+"""
+Pomodoro Display System
+
+A self-hosted Pomodoro timer with fullscreen display and remote control.
+"""
+
 from ._version import version as __version__  # noqa
+from .app import app, main
+
+__all__ = ["app", "main"]
